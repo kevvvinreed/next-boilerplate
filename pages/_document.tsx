ac5c1fec7dbf5d5a9074f06a2f9a6d0c944393a6
Kevin Reed <kevvvinreed@gmail.com>
2025-08-11 09:26:21 -0700

    theme function needs to be string now

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,35 @@ export default class MyDocument extends Document {
         <body>
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function() {${setInitialColorMode.toString()}setInitialColorMode();})()`,
+              __html: `
+                (function() {
+                  function getInitialColorMode() {
+                    const persistedPreferenceMode = window.localStorage.getItem('theme');
+                    const hasPersistedPreference = typeof persistedPreferenceMode === 'string';
+
+                    if (hasPersistedPreference) {
+                      return persistedPreferenceMode;
+                    }
+
+                    const preference = window.matchMedia('(prefers-color-scheme: dark)');
+                    const hasMediaQueryPreference = typeof preference.matches === 'boolean';
+
+                    if (hasMediaQueryPreference) {
+                      return preference.matches ? 'dark' : 'light';
+                    }
+
+                    return 'light';
+                  }
+
+                  const currentColorMode = getInitialColorMode();
+                  const element = document.documentElement;
+                  element.style.setProperty('--initial-color-mode', currentColorMode);
+
+                  if (currentColorMode === 'dark') {
+                    document.documentElement.setAttribute('data-theme', 'dark');
+                  }
+                })();
+              `,
             }}
           />
           <Main />
@@ -19,33 +47,5 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     );
-  }
-}
-
-function setInitialColorMode() {
-  function getInitialColorMode() {
-    const persistedPreferenceMode = window.localStorage.getItem('theme');
-    const hasPersistedPreference = typeof persistedPreferenceMode === 'string';
-
-    if (hasPersistedPreference) {
-      return persistedPreferenceMode;
-    }
-
-    const preference = window.matchMedia('(prefers-color-scheme: dark)');
-    const hasMediaQueryPreference = typeof preference.matches === 'boolean';
-
-    if (hasMediaQueryPreference) {
-      return preference.matches ? 'dark' : 'light';
-    }
-
-    return 'light';
-  }
-
-  const currentColorMode = getInitialColorMode();
-  const element = document.documentElement;
-  element.style.setProperty('--initial-color-mode', currentColorMode);
-
-  if (currentColorMode === 'dark') {
-    document.documentElement.setAttribute('data-theme', 'dark');
   }
 }
